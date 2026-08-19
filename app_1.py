4dd843c043d9715ec7cc4e694ccf3e337a3b8b5c
richard663-ui <1910067136@qq.com>
2026-08-19 22:52:18 +0800

    Point Streamlit entrypoint to V18 final dashboard

--- app_1.py
+++ app_1.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-"""Streamlit deployment entrypoint for V18 Level-2 branch."""
-from app_v18 import *  # noqa: F401,F403
+"""Streamlit deployment entrypoint for V18 Final."""
+from app_v18_final import *  # noqa: F401,F403
